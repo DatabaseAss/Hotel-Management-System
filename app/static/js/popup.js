e9b35ebedbcdd32db83363097fb3e97c02328f84
chanhtran2410 <tranletrungchanh12345@gmail.com>
2022-11-23 14:47:37 +0700

    update room

--- app/static/js/popup.js
+++ app/static/js/popup.js
@@ -1,4 +1,16 @@
 function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
-  }
+}
+function myFunction1() {
+  var popup = document.getElementById("myPopup1");
+  popup.classList.toggle("show");
+}
+function myFunction2() {
+  var popup = document.getElementById("myPopup2");
+  popup.classList.toggle("show");
+}
+function myFunction3() {
+  var popup = document.getElementById("myPopup3");
+  popup.classList.toggle("show");
+}
